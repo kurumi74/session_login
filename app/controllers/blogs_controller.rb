663ac25c3b2e992321e90962a891718fbfd11518
kurumi <krkrteikoku@gmail.comi>
2020-07-14 22:33:52 +0900

    アソシエーション3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,11 +6,35 @@ class BlogsController < ApplicationController
         @blog = Blog.new
     end
     def create
-        Blog.create(blog_params)
-        redirect_to new_blog_path
-    end
+        @blog = current_user.blogs.build(blog_params)
+        if @blog.save
+          redirect_to blogs_path, notice: "ブログを作成しました！"
+        else
+          render :new
+        end
+      end
     def show
+        Blog.find(params[:id])
     end
+    def edit
+        @blog = Blog.find(params[:id])
+    end
+    def update
+        @blog = Blog.find(params[:id])
+        if @blog.update(blog_params)
+          redirect_to blogs_path, notice: "ブログを編集しました！"
+        else
+          render :edit
+        end
+    end
+    def destroy
+        @blog.destroy
+        redirect_to blogs_path, notice:"ブログを削除しました！"
+    end
+    def confirm
+        @blog = current_user.blogs.build(blog_params)
+        render :new if @blog.invalid?
+      end
     private
     def blog_params
       params.require(:blog).permit(:title, :content)
